354bc7086d2031c1a834dfb7ea40490b8589cec2
hoho <ghdnjs18@naver.com>
2024-04-25 17:07:19 +0900

    Querydsl 지원 클래스 직접 만들기

---
+++ src/main/java/study/querydsl/repository/MemberTestRepository.java
@@ -0,0 +1,101 @@
+package study.querydsl.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.JPQLQuery;
+import com.querydsl.jpa.impl.JPAQuery;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.stereotype.Repository;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.repository.support.Querydsl4RepositorySupport;
+
+import java.util.List;
+
+import static org.springframework.util.StringUtils.hasText;
+import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
+
+@Repository
+public class MemberTestRepository extends Querydsl4RepositorySupport {
+
+    public MemberTestRepository() {
+        super(Member.class);
+    }
+
+    public List<Member> basicSelect() {
+        return select(member)
+                .from(member)
+                .fetch();
+    }
+
+    public List<Member> basicSelectFrom() {
+        return selectFrom(member)
+                .fetch();
+    }
+
+    public Page<Member> searchPageByApplyPage(MemberSearchCondition condition, Pageable pageable) {
+        JPAQuery<Member> query = selectFrom(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()));
+
+        List<Member> content = getQuerydsl().applyPagination(pageable, query).fetch();
+
+        return PageableExecutionUtils.getPage(content, pageable, query::fetchCount);
+    }
+
+    public Page<Member> applyPagination(MemberSearchCondition condition, Pageable pageable) {
+        return applyPagination(pageable, query -> query
+                .selectFrom(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())));
+    }
+
+    public Page<Member> applyPagination2(MemberSearchCondition condition, Pageable pageable) {
+        return applyPagination(pageable, query -> query
+                .selectFrom(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                ), countQuery -> countQuery
+                        .select(member.id.count())
+                        .from(member)
+                        .leftJoin(member.team, team)
+                        .where(
+                                usernameEq(condition.getUsername()),
+                                teamNameEq(condition.getTeamName()),
+                                ageGoe(condition.getAgeGoe()),
+                                ageLoe(condition.getAgeLoe()))
+                );
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoeCond) {
+        return ageGoeCond == null ? null : member.age.goe(ageGoeCond);
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoeCond) {
+        return ageLoeCond == null ? null : member.age.loe(ageLoeCond);
+    }
+
+    private BooleanExpression teamNameEq(String teamNameCond) {
+        return !hasText(teamNameCond) ? null : team.name.eq(teamNameCond);
+    }
+
+    private BooleanExpression usernameEq(String usernameCond) {
+        return !hasText(usernameCond) ? null : member.username.eq(usernameCond);
+    }
+}
